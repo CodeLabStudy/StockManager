a5951bd03fe2175b50856ff33e0d89be3f28f8a7
iseverton <iseverton2024@gmail.com>
2025-04-08 16:17:29 -0300

    feat: melhorias no frontEnd

--- src/js/tab.js
+++ src/js/tab.js
@@ -1,12 +1,27 @@
 function InitTabs() {
+  const sidebar = document.getElementById("sidebar");
+  const toggleButton = document.getElementById("sidebar-toggle");
+  toggleButton.addEventListener("click", (e) => {
+    e.stopPropagation();
+    sidebar.classList.toggle("collapsed");
+  });
   const tabLinks = document.querySelectorAll(".tab-link");
   const tabContents = document.querySelectorAll(".tab-content");
-  console.log(tabContents);
+  const tituloHeader = document.getElementById("titulo-header");
   tabLinks.forEach((link) => {
     link.addEventListener("click", (e) => {
+      e.preventDefault();
       console.log(e.target);
+
       const tabId = link.dataset.tab;
-      console.log(tabId);
+      if (tabId === "estoque") {
+        tituloHeader.textContent = "Gerenciamento de Estoque";
+      } else if (tabId === "fornecedores") {
+        tituloHeader.textContent = "Gerenciamento de Fornecedores";
+      } else if (tabId === "graficos") {
+        tituloHeader.textContent = "Análise de Dados";
+      }
+      // console.log(tabId);
       tabLinks.forEach((link) => link.classList.remove("active"));
       tabContents.forEach((content) => content.classList.remove("active"));
       link.classList.add("active");
