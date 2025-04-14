7e4b55473a10c8607dd9c4c1f582233b0821eff4
haniel23 <hanieltenoriomelo@hotmail.comgit config --global user.email hanieltenoriomelo@hotmail.com>
2025-04-14 09:04:13 -0300

    feat: cadastrar produto

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,11 @@
 import { InitTabs } from "./tab.js";
 import CadastrarFornecedor from "./forms/fornecedorForm.js";
-<<<<<<< HEAD
 import CadastrarProduto from "./forms/produtosForm.js";
+import { renderFornecedores } from "./renderFornecedores.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   InitTabs();
   CadastrarFornecedor();
-  CadastrarProduto();
-=======
-import { renderFornecedores } from "./renderFornecedores.js";
-document.addEventListener("DOMContentLoaded", () => {
-  InitTabs();
-  CadastrarFornecedor();
   renderFornecedores();
->>>>>>> cc956dc2f5cc6ac82d0951b635ce5049d57f6e9c
+  CadastrarProduto();
 });
